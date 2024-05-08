598681967af52f0c49f8d8d0b68500bc991e4135
Nuria.Bravo <nabz@gft.com>
2024-05-08 12:07:51 +0200

    Added Cart Model

--- src/main/java/com/gftworkshop/cartMicroservice/model/Cart.java
+++ src/main/java/com/gftworkshop/cartMicroservice/model/Cart.java
@@ -1,6 +1,49 @@
 package com.gftworkshop.cartMicroservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Temporal;
+
+import java.util.Date;
+
+@Entity
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
     public Cart() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
