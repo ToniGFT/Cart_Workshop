71eb98cd05702d87e2d3513532e7faee9491cb7c
GABRIEL <gldf@gft.com>
2024-05-08 14:38:49 +0200

    Added CartDto tests and CartProductDto tests

--- src/test/java/com/gftworkshop/cartMicroservice/api/dto/CartDtoTest.java
+++ src/test/java/com/gftworkshop/cartMicroservice/api/dto/CartDtoTest.java
@@ -2,6 +2,27 @@ package com.gftworkshop.cartMicroservice.api.dto;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CartDtoTest {
+
+    @Test
+    public void testGetAndSetId() {
+        CartDto cart = new CartDto();
+        cart.setId(1L);
+        assertEquals(1L, cart.getId());
+    }
+
+    @Test
+    public void testGetAndSetCartProducts() {
+        CartDto cart = new CartDto();
+        List<CartProductDto> cartProducts = new ArrayList<>();
+        cartProducts.add(new CartProductDto());
+        cart.setCartProducts(cartProducts);
+
+        assertEquals(1, cart.getCartProducts().size());
+    }
 }
+
