9fcbc0b1da240ba315a913005d6315947f7aaf5b
Ivan Perez <inpr@gft.com>
2024-05-08 16:18:22 +0200

    CartDto implemented and Cart class (entity) modified

--- src/main/java/com/gftworkshop/cartMicroservice/api/dto/CartDto.java
+++ src/main/java/com/gftworkshop/cartMicroservice/api/dto/CartDto.java
@@ -1,4 +1,17 @@
 package com.gftworkshop.cartMicroservice.api.dto;
 
+import com.gftworkshop.cartMicroservice.model.CartProduct;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Builder
 public class CartDto {
+    private Long id;
+    private Long user_id;
+    private Date updated_at;
+    private List<CartProduct> cartProducts;
 }
--- src/main/java/com/gftworkshop/cartMicroservice/model/Cart.java
+++ src/main/java/com/gftworkshop/cartMicroservice/model/Cart.java
@@ -1,35 +1,36 @@
 package com.gftworkshop.cartMicroservice.model;
 
 
+import com.gftworkshop.cartMicroservice.api.dto.CartDto;
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
 
+import java.util.Date;
 import java.util.List;
 
+@Data
 @Entity
+@Builder
 @Table(name = "cart")
 public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name="user_id")
+    private Long user_id;
+
+    @Column(name="updated_at")
+    private Date updated_at;
+
     @OneToMany(mappedBy = "cart")
     private List<CartProduct> cartProducts;
 
     public Cart() {}
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public List<CartProduct> getCartProducts() {
-        return cartProducts;
-    }
-
-    public void setCartProducts(List<CartProduct> cartProducts) {
-        this.cartProducts = cartProducts;
-    }
+//    public static CartDto fromEntity(Cart entity){
+//        return Cart.builder()
+//                .id()
+//    }
 }
