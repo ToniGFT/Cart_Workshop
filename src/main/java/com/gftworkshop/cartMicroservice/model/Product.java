cb129406a0afc602213be9dd12b884f048439842
GABRIEL <gldf@gft.com>
2024-05-08 15:09:43 +0200

    Added to product and added Product tests

--- src/main/java/com/gftworkshop/cartMicroservice/model/Product.java
+++ src/main/java/com/gftworkshop/cartMicroservice/model/Product.java
@@ -1,4 +1,71 @@
 package com.gftworkshop.cartMicroservice.model;
 
+import java.math.BigDecimal;
+
 public class Product {
+    private Long productId;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Long stock;
+    private BigDecimal discount;
+    private String category;
+
+    public Product() {}
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Long getStock() {
+        return stock;
+    }
+
+    public void setStock(Long stock) {
+        this.stock = stock;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
