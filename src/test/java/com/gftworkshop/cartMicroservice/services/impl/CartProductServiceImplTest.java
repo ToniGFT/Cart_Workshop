0680a4343eabfa80c6e0e96464ea2001f74e0515
Ivan Perez <inpr@gft.com>
2024-05-09 19:32:58 +0200

    CartProductServiceImplTest modified

--- src/test/java/com/gftworkshop/cartMicroservice/services/impl/CartProductServiceImplTest.java
+++ src/test/java/com/gftworkshop/cartMicroservice/services/impl/CartProductServiceImplTest.java
@@ -1,14 +1,10 @@
 package com.gftworkshop.cartMicroservice.services.impl;
 
-import com.gftworkshop.cartMicroservice.api.dto.Product;
 import com.gftworkshop.cartMicroservice.model.CartProduct;
 import com.gftworkshop.cartMicroservice.repositories.CartProductRepository;
 import com.gftworkshop.cartMicroservice.services.CartProductService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-
-import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -19,7 +15,6 @@ public class CartProductServiceImplTest {
     private CartProductRepository cartProductRepository;
     private CartProductService cartProductService;
     private CartProduct cartProduct;
-    private Product product;
 
     @BeforeEach
     void setUp() {
@@ -43,25 +38,4 @@ public class CartProductServiceImplTest {
         verify(cartProductRepository).save(any(CartProduct.class));
     }
 
-    @Test
-    void productNameIsAssignedCorrectly() {
-
-        cartProduct.setProductName("Airmax");
-        cartProductService.save(cartProduct);
-
-        ArgumentCaptor<CartProduct> cartProductCaptor = ArgumentCaptor.forClass(CartProduct.class);
-        verify(cartProductRepository).save(cartProductCaptor.capture());
-
-        CartProduct savedCartProduct = cartProductCaptor.getValue();
-
-        assertEquals("Airmax", savedCartProduct.getProductName());
-
-    }
-
-    @Test
-    void productFieldAreSavedCorrectly() {
-
-
-
-    }
 }
