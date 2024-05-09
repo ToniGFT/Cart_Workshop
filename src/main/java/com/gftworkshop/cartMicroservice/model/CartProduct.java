0594533018df607529b66b8076a8c00a3cd602b4
Nuria.Bravo <nabz@gft.com>
2024-05-09 15:36:11 +0200

    Fixed CartProduct

--- src/main/java/com/gftworkshop/cartMicroservice/model/CartProduct.java
+++ src/main/java/com/gftworkshop/cartMicroservice/model/CartProduct.java
@@ -30,67 +30,16 @@ public class CartProduct {
     @Column(name = "price", precision = 10, scale = 2)
     private BigDecimal price;
 
-    // Constructor, Getters and Setters
     public CartProduct() {
     }
 
-    public CartProduct(Cart cart) {
-        this.cart = cart;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
+    public CartProduct(Long id, Cart cart, String productName, String productCategory, String productDescription, Integer quantity, BigDecimal price) {
         this.id = id;
-    }
-
-    public Cart getCart() {
-        return cart;
-    }
-
-    public void setCart(Cart cart) {
         this.cart = cart;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
         this.productName = productName;
-    }
-
-    public String getProductCategory() {
-        return productCategory;
-    }
-
-    public void setProductCategory(String productCategory) {
         this.productCategory = productCategory;
-    }
-
-    public String getProductDescription() {
-        return productDescription;
-    }
-
-    public void setProductDescription(String productDescription) {
         this.productDescription = productDescription;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
         this.quantity = quantity;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
         this.price = price;
     }
 
