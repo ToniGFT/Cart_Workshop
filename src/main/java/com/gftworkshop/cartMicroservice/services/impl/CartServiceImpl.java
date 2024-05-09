abec0f207a1ff3bf1038acc5f6b1d543bc68238d
GABRIEL <gldf@gft.com>
2024-05-09 08:44:22 +0200

    Added h2 data

--- src/main/java/com/gftworkshop/cartMicroservice/services/impl/CartServiceImpl.java
+++ src/main/java/com/gftworkshop/cartMicroservice/services/impl/CartServiceImpl.java
@@ -1,6 +1,8 @@
 package com.gftworkshop.cartMicroservice.services.impl;
 
 import com.gftworkshop.cartMicroservice.services.CartService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CartServiceImpl implements CartService {
 }
