47acde260c761f80501a649e636e7617917bcaa4
GABRIEL <gldf@gft.com>
2024-05-09 07:58:53 +0200

    Replaced CartProduct getters, setters, and constructors with Lombok

--- src/main/java/com/gftworkshop/cartMicroservice/model/CartProduct.java
+++ src/main/java/com/gftworkshop/cartMicroservice/model/CartProduct.java
@@ -1,9 +1,13 @@
 package com.gftworkshop.cartMicroservice.model;
 
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
 
 import java.math.BigDecimal;
 
+@Data
+@Builder
 @Entity
 @Table(name = "cart_products")
 public class CartProduct {
@@ -30,62 +34,5 @@ public class CartProduct {
     @Column(name = "price", precision = 10, scale = 2)
     private BigDecimal price;
 
-    // Constructor, Getters and Setters
-    public CartProduct() {}
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Cart getCart() {
-        return cart;
-    }
-
-    public void setCart(Cart cart) {
-        this.cart = cart;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public String getProductCategory() {
-        return productCategory;
-    }
-
-    public void setProductCategory(String productCategory) {
-        this.productCategory = productCategory;
-    }
-
-    public String getProductDescription() {
-        return productDescription;
-    }
-
-    public void setProductDescription(String productDescription) {
-        this.productDescription = productDescription;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
 }
