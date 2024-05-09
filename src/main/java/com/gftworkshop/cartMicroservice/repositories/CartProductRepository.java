e16791434c76ea5831c1ab9b121d6c0378bb86f3
Nuria.Bravo <nabz@gft.com>
2024-05-09 15:29:56 +0200

    TDD CartProductRepository, CartRepository and Fixed CartProduct

--- src/main/java/com/gftworkshop/cartMicroservice/repositories/CartProductRepository.java
+++ src/main/java/com/gftworkshop/cartMicroservice/repositories/CartProductRepository.java
@@ -2,6 +2,10 @@ package com.gftworkshop.cartMicroservice.repositories;
 
 import com.gftworkshop.cartMicroservice.model.CartProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CartProductRepository extends JpaRepository<CartProduct, Long> {
+
+    @Query("UPDATE CartProduct cp SET cp.quantity = ?2 WHERE cp.id = ?1")
+    int updateProductQuantity(Long productId, int newQuantity);
 }
